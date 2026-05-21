afd869d0a09ad8fa8a6edbbb608c79385867e800
yannickRafael <yannickrafael286@gmail.com>
2026-05-21 17:51:00 +0200

    add Document model with schema and indexes

---
+++ src/models/Document.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const DocumentSchema = mongoose.Schema({
+    projectId: {type:mongoose.Schema.Types.ObjectId, ref:'Project'},
+    title: {type: String, required: true},
+    documentType: String,
+    format: {type: String, default: 'application/pdf'},
+    url: String,
+    datePublished: String
+}, {timestamps:true});
+
+DocumentSchema.index({projectId:1});
+
+module.exports = mongoose.model('Document', DocumentSchema);

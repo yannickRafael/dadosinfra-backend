e6c167381dfdfd8a8a9b8a576298545f52e78da4
yannickRafael <yannickrafael286@gmail.com>
2026-05-21 17:31:48 +0200

    add Tender model with schema and indexes

---
+++ src/models/Tender.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const TenderSchema = new mongoose.Schema({
+    projectId: {type: mongoose.Schema.Types.ObjectId, required:true, ref:'Project'},
+    title:{type: String, required: true},
+    status: {type: String, enum:['planned', 'active', 'complete', 'cancelled']},
+    procurementMethod:{type: String, enum:['open', 'limited', 'selective', 'direct']},
+    value: {amount: Number, currency: String},
+    tenderPeriod: {startDate: String, endDate:String}
+}, {timestamps:true});
+
+TenderSchema.index({projectId:1});
+
+module.exports = mongoose.model('Tender', TenderSchema)
